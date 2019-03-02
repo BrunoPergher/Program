10e9f920fda48addef3e175d2497cc2310f10b37
BrunoPergher <brunopergher_1@hotmail.com>
2019-03-01 23:13:35 -0300

    lista1e2

---
+++ Programação 1/src/br/edu/ifcvideira/Lista2/Ex8.java
@@ -0,0 +1,51 @@
+package br.edu.ifcvideira.Lista2;
+
+import java.util.Scanner;
+
+//Ler 3 valores e escrevê-los em ordem crescente (Considerando valores distintos).
+public class Ex8 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		double v1;
+		double v2;
+		double v3;
+
+		System.out.println(" Indique um numero ");
+		v1 = Double.parseDouble(teclado.nextLine());
+
+		System.out.println(" Indique outro numero ");
+		v2 = Double.parseDouble(teclado.nextLine());
+
+		System.out.println(" Indique mais um numero ");
+		v3 = Double.parseDouble(teclado.nextLine());
+
+		if( v1 < v2 && v2 < v3 ) {
+
+			System.out.println ( v1 + " " + v2 + " " + v3);
+
+		} else if( v1 < v3 && v3 < v2 ) {
+
+			System.out.println ( v1 + " " + v3 + " " + v2);
+
+		} else if( v2 < v1 && v1 < v3 ) {
+
+			System.out.println ( v2 + " " + v1 + " " + v3);
+
+		} else if( v2 < v3 && v3 < v1 ) {
+
+			System.out.println ( v2 + " " + v3 + " " + v1);
+
+		} else if( v3 < v2 && v2 < v1 ) {
+
+			System.out.println ( v3 + " " + v2 + " " + v1);
+
+		} else  {
+
+			System.out.println ( v3 + " " + v1 + " " + v2);
+
+		}
+		teclado.close();
+	}
+
+	}
